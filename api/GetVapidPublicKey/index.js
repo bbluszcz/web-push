637b0928dc0e564f718102cf0b2a0e4c9fe3565e
Bartlomiej Bluszcz <31951238+bbluszcz@users.noreply.github.com>
2023-09-20 13:52:13 +0200

    Update index.js

--- api/GetVapidPublicKey/index.js
+++ api/GetVapidPublicKey/index.js
@@ -12,13 +12,13 @@ module.exports = async function (context, req) {
   client.trackEvent({
     name: 'get_vapid-public-key_success',
     tagOverrides: operationIdOverride,
-    'vapid-public-key': process.env.VAPID_PUBLIC_KEY,
+    'vapid-public-key': BJ_BKIUDluNnkG3MJbH_RJ4DMo8dutMOsOlKyewLmVPvGSzl4Pun996r2gMGoeJIi2jgRgStwALc3sd-3shzgkc,
   });
 
   context.res = {
     body: {
       message: 'get_vapid-public-key_success',
-      'vapid-public-key': process.env.VAPID_PUBLIC_KEY,
+      'vapid-public-key': BJ_BKIUDluNnkG3MJbH_RJ4DMo8dutMOsOlKyewLmVPvGSzl4Pun996r2gMGoeJIi2jgRgStwALc3sd-3shzgkc,
     },
   };
 };
